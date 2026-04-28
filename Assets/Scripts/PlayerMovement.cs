c3967a371febcc65fd45caac0198f491d36933b9
kerhard-153 <kerhard@student.cscc.edu>
2026-04-27 22:26:23 -0400

    add bee

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,6 +10,7 @@ using System.Xml;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public PauseManager pauseManager;
 
     [Header("Movement")]
     public float moveSpeed = 5f;
@@ -17,17 +18,34 @@ public class PlayerMovement : MonoBehaviour
 
     [Header("Jumping")]
     public float jumpPower = 10f;
+    public int maxJumps = 2;
+    int jumpsRemaining;
 
     [Header("GroundCheck")]
     public Transform groundCheckPos;
     public Vector2 groundCheckSize = new Vector2(0.5f, 0.5f);
     public LayerMask groundLayer;
+    private bool isGrounded;
 
+    [Header("Fall Damage")]
+    public float fallDamageThreshold = -5f;
+    private bool wasGrounded;
+    private float minYVelocity;
+    private bool hasTakenFallDamage;
+
+    public GameObject menuUI;
+    private bool isMenuOpen = false;
     public Animator anim;
 
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
     void Start()
     {
         anim = GetComponent<Animator>();
+        Time.timeScale = 1f;
     }
 
     // Update is called once per frame
@@ -35,13 +53,70 @@ public class PlayerMovement : MonoBehaviour
     {
         rb.linearVelocity = new Vector2(horizontalMovement * moveSpeed, rb.linearVelocity.y);
         anim.SetBool("isMoving", horizontalMovement != 0);
+        anim.SetBool("isGrounded", isGrounded);
 
         if (horizontalMovement > 0) {
             transform.localScale = new Vector3(1, 1, 1);
         } else if (horizontalMovement < 0) {
             transform.localScale = new Vector3(-1, 1, 1);
         }
+        
+        GroundCheck();
+
+        
     }
+
+    void FixedUpdate()
+    {
+        float currentY = rb.linearVelocity.y;
+
+        if (!isGrounded)
+        {
+            if (currentY < minYVelocity)
+            {
+                minYVelocity = currentY;
+            }
+        }
+    }
+
+    private void GroundCheck()
+    {
+        isGrounded = Physics2D.OverlapBox(groundCheckPos.position, groundCheckSize, 0, groundLayer);
+
+        // just left the ground
+        if (!isGrounded && wasGrounded)
+        {
+            minYVelocity = 0;
+            hasTakenFallDamage = false;
+        }
+
+        // tracks fall speed
+        if (!isGrounded)
+        {
+            float currentY = rb.linearVelocity.y;
+            if (currentY < minYVelocity)
+            {
+                minYVelocity = currentY;
+            }
+        }
+
+        // landed
+        if (isGrounded && !wasGrounded)
+        {
+            if (!hasTakenFallDamage && minYVelocity < fallDamageThreshold)
+            {
+                GetComponent<PlayerHealth>().TakeDamage(1);
+                hasTakenFallDamage = true;
+            }
+        }
+        if (isGrounded)
+        {
+            jumpsRemaining = maxJumps;
+        }
+        wasGrounded = isGrounded;
+
+    }
+
 
     public void Move(InputAction.CallbackContext context)
     {
@@ -50,26 +125,39 @@ public class PlayerMovement : MonoBehaviour
 
     public void Jump(InputAction.CallbackContext context)
     {
-        if (isGrounded())
-        {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpPower);
-        }
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpPower);
+            jumpsRemaining --;
+
+            anim.SetTrigger("isJumping");
+
+            if (jumpsRemaining == 1)
+            {
+                // play particles
+            }
         
     }
-
-    private bool isGrounded()
-    {
-        if (Physics2D.OverlapBox(groundCheckPos.position, groundCheckSize, 0, groundLayer))
-        {
-            return true;
-        }
-        return false;
-    }
-
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.white;
         Gizmos.DrawWireCube(groundCheckPos.position, groundCheckSize);
+    }
+
+    public void Pause(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Debug.Log("PAUSE PRESSED");
+            pauseManager.TogglePause();
+        }
+    }
+
+    public void ToggleMenu(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            isMenuOpen = !isMenuOpen;
+            menuUI.SetActive(isMenuOpen);
+        }
     }
 
 }
