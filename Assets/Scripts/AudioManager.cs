ade9d138e5659d46a77aba9777ed70fa82a5742b
kerhard-153 <kerhard@student.cscc.edu>
2026-04-27 20:16:41 -0400

    added rain to scene

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+
+    public AudioSource sfxSource;
+    public AudioSource musicSource;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void PlaySFX(AudioClip clip)
+    {
+        sfxSource.PlayOneShot(clip);
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,42 +11,23 @@ public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D rb;
 
-    public PauseManager pauseManager;
-
     [Header("Movement")]
     public float moveSpeed = 5f;
     float horizontalMovement;
 
     [Header("Jumping")]
     public float jumpPower = 10f;
-    public int maxJumps = 2;
-    int jumpsRemaining;
 
     [Header("GroundCheck")]
     public Transform groundCheckPos;
     public Vector2 groundCheckSize = new Vector2(0.5f, 0.5f);
     public LayerMask groundLayer;
-    private bool isGrounded;
 
-    [Header("Fall Damage")]
-    public float fallDamageThreshold = -5f;
-    private bool wasGrounded;
-    private float minYVelocity;
-    private bool hasTakenFallDamage;
-
-
-    public GameObject menuUI;
-    private bool isMenuOpen = false;
     public Animator anim;
 
-    void Awake()
-    {
-        DontDestroyOnLoad(gameObject);
-    }
     void Start()
     {
         anim = GetComponent<Animator>();
-        Time.timeScale = 1f;
     }
 
     // Update is called once per frame
@@ -54,29 +35,11 @@ public class PlayerMovement : MonoBehaviour
     {
         rb.linearVelocity = new Vector2(horizontalMovement * moveSpeed, rb.linearVelocity.y);
         anim.SetBool("isMoving", horizontalMovement != 0);
-        anim.SetBool("isGrounded", isGrounded);
 
         if (horizontalMovement > 0) {
             transform.localScale = new Vector3(1, 1, 1);
         } else if (horizontalMovement < 0) {
             transform.localScale = new Vector3(-1, 1, 1);
-        }
-
-        GroundCheck();
-
-        
-    }
-
-    void FixedUpdate()
-    {
-        float currentY = rb.linearVelocity.y;
-
-        if (!isGrounded)
-        {
-            if (currentY < minYVelocity)
-            {
-                minYVelocity = currentY;
-            }
         }
     }
 
@@ -87,83 +50,26 @@ public class PlayerMovement : MonoBehaviour
 
     public void Jump(InputAction.CallbackContext context)
     {
-        if (jumpsRemaining > 0)
+        if (isGrounded())
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpPower);
-            jumpsRemaining --;
-
-            anim.SetTrigger("isJumping");
-
-            if (jumpsRemaining == 1)
-            {
-                // play particles
-            }
         }
         
     }
 
-    private void GroundCheck()
+    private bool isGrounded()
     {
-        isGrounded = Physics2D.OverlapBox(groundCheckPos.position, groundCheckSize, 0, groundLayer);
-
-        // just left the ground
-        if (!isGrounded && wasGrounded)
+        if (Physics2D.OverlapBox(groundCheckPos.position, groundCheckSize, 0, groundLayer))
         {
-            minYVelocity = 0;
-            hasTakenFallDamage = false;
+            return true;
         }
-
-        // tracks fall speed
-        if (!isGrounded)
-        {
-            float currentY = rb.linearVelocity.y;
-            if (currentY < minYVelocity)
-            {
-                minYVelocity = currentY;
-            }
-        }
-
-        // landed
-        if (isGrounded && !wasGrounded)
-        {
-            if (!hasTakenFallDamage && minYVelocity < fallDamageThreshold)
-            {
-                GetComponent<PlayerHealth>().TakeDamage(1);
-                hasTakenFallDamage = true;
-            }
-        }
-
-        if (isGrounded)
-        {
-            jumpsRemaining = maxJumps;
-        }
-
-        wasGrounded = isGrounded;
-
+        return false;
     }
 
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.white;
         Gizmos.DrawWireCube(groundCheckPos.position, groundCheckSize);
-    }
-
-    public void Pause(InputAction.CallbackContext context)
-    {
-        if (context.performed)
-        {
-            Debug.Log("PAUSE PRESSED");
-            pauseManager.TogglePause();
-        }
-    }
-
-    public void ToggleMenu(InputAction.CallbackContext context)
-    {
-        if (context.performed)
-        {
-            isMenuOpen = !isMenuOpen;
-            menuUI.SetActive(isMenuOpen);
-        }
     }
 
 }
